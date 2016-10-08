8c26acf49622e6e32e47367a9d8d1de17e4d8df6
EsterKais <kaisester91@gmail.com>
2016-10-08 10:08:15 +0200

    Working IndexShow

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -1,7 +1,50 @@
 class BeansController < ApplicationController
 
   def index
-    @beans = Bean.all
+    @beans = Bean.all #- we do need this, if we go with my new idea. We want it to display all the beans that we have, but just display the roast. we probably do not need this as we dont want to see all the beans anyway
   end
 
-end #end of class 
+  def show
+    @bean = Bean.find(params[:id])
+  end
+
+  def new
+    @bean = Bean.new
+  end
+
+  def create
+    @bean = Bean.new( bean_params )
+      if @bean.save
+        redirect_to @bean
+      else
+        render 'new'
+      end
+    end  #create method end
+
+    def edit
+      @bean = Bean.find(params[:id])
+    end
+
+    def update
+      @bean = Bean.find(params[:id])
+      if @bean.update_attributes( bean_params )
+        redirect_to @bean
+      else
+        render 'edit'
+      end
+    end #of method update
+
+    def destroy
+      @bean = Bean.find(params[:id])
+      @bean.destroy
+      redirect_to beans_path
+    end #of method destroy
+
+  private
+
+    def bean_params
+      params.require(:bean).permit(:name, :roast, :flavour, :country, :image_url, :price)
+    end #of method bean_params
+
+
+end #end of class
